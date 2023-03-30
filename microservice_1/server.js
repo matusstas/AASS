c58d088ba7b0eba534701729fbbda31508f58b88
Matúš Staš <matusstas.ms@gmail.com>
2023-03-30 19:25:18 +0200

    Update structure

--- microservice_1/server.js
+++ microservice_1/server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
+const todo = require('./routes/todo.js')
 
 const PORT = 80;
 const HOST = "0.0.0.0";
 
 const app = express();
+
+app.use("/todo", todo);
 
 app.get('/', (req, res) => {
    res.send('Hello from Microservice 1');
